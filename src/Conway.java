987071ba226a7b0480b4d47b0356c15d122b12d3
Arief Saferman <arief.saferman@gmail.com>
2021-09-18 20:34:02 +0700

    create basic grid

--- src/Conway.java
+++ src/Conway.java
@@ -1,7 +1,38 @@
 
 public class Conway
 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args)
+    {
+        CreateGrid();
+    }
+
+    public static void CreateGrid()
+    {
+        int M = 10;
+        int N = 10;
+
+        //Create basic simple grid
+        int[][] grid = {
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
+                { 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+        };
+
+        for (int i = 0; i < M; i++){
+            for (int j = 0; j < N; j++){
+                if (grid[i][j] == 0)
+                    System.out.print(".");
+                else
+                    System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
