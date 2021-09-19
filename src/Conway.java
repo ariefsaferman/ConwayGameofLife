1c0b4492de2ce3f9386e038dad64d3219e6e4e06
Arief Saferman <arief.saferman@gmail.com>
2021-09-19 15:02:27 +0700

    Add Neighbour

--- src/Conway.java
+++ src/Conway.java
@@ -3,10 +3,12 @@ public class Conway
 {
     public static void main(String[] args)
     {
-        CreateGrid();
+        InitiateGrid();
+
+
     }
 
-    public static void CreateGrid()
+    public static void InitiateGrid()
     {
         int M = 10;
         int N = 10;
@@ -33,6 +35,26 @@ public class Conway
                     System.out.print("*");
             }
             System.out.println();
+        }
+        System.out.println();
+        nextGen(grid, M, N);
+
+    }
+
+    static void nextGen(int grid[][], int M, int N)
+    {
+        int[][] future = new int[M][N];
+
+        for (int i = 1; i < M - 1; i++) {
+            for (int j = 1; j < N - 1; j++) {
+                int lifeNeighbour = 0;
+                for (int l = -1; l <= 1; l++) {
+                    for (int m = -1; m <= 1; m++) {
+                        lifeNeighbour += grid[i+l][j+m];
+                        lifeNeighbour -= grid[i][j];
+                    }
+                }
+            }
         }
     }
 }
